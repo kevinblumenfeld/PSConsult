d378d2392ade3ac32bc65d1aa6657a66af280110
kevinblumenfeld <github@thenext.net>
2017-09-01 19:57:15 -0400

    Need to fix the DN to sAMAccountname conversion.. use DN instead for that

--- PSConsult/Get-PermissionsFA.ps1
+++ PSConsult/Get-PermissionsFA.ps1
@@ -20,7 +20,7 @@
                 } | Select -ExpandProperty User) -join "*"
             if ($FullAccess) {
                 ($FullAccess).split("*") | % {
-                    $ADType = (Get-ADAccountType -name $_)
+                    $ADType = (Get-ADAccountType -name $_ -FA)
                     if ($ADType -eq 'User') {
                         $FAHash = @{}
                         $FAHash['Mailbox'] = ($Mailbox.DisplayName)
@@ -61,6 +61,67 @@
     }
 }
 
+function Get-PermissionsSA {
+    [CmdletBinding()]
+    Param 
+    (
+
+    )
+    Begin {
+
+    }
+    Process {
+        Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010 -EA SilentlyContinue
+        Set-AdServerSettings -ViewEntireForest $true
+        $resultArray = @()
+        $Mailboxes = Get-Mailbox -ResultSize:Unlimited | Select DistinguishedName, UserPrincipalName, DisplayName, Alias,
+        @{n = "OU" ; e = {$_.Distinguishedname | ForEach-Object {($_ -split '(OU=)', 2)[1, 2] -join ''}}}
+        
+        ForEach ($Mailbox in $Mailboxes) { 
+            $SendAs = (Get-RecipientPermission $Mailbox.DistinguishedName | ? {$_.AccessRights -match "SendAs" -and $_.Trustee -ne "NT AUTHORITY\SELF" -and !$_.Trustee.tostring().startswith('S-1-5-21-')} | select -ExpandProperty trustee)
+            if ($SendAs) {
+                ($SendAs).split("*") | % {
+                    $ADType = (Get-ADAccountType -name $_ -SA)
+                    if ($ADType -eq 'User') {
+                        $SAHash = @{}
+                        $SAHash['Mailbox'] = ($Mailbox.DisplayName)
+                        try {
+                            $SAHash['SendAs'] = ((Get-Mailbox $_ -ErrorAction SilentlyContinue).DisplayName)
+                            if ($SAHash.SendAs) {
+                                $resultArray += [psCustomObject]$SAHash
+                            }
+                        }
+                        Catch {
+
+                        }
+                    }
+                    if ($ADType -eq 'Group') {
+                        if ($_.Contains('\')) {
+                            $Name = $_.Split('\')[1]
+                        }
+                        Get-ADGroupMember $Name -Recursive | % {
+                            $SAHash = @{}
+                            $SAHash['Mailbox'] = ($Mailbox.DisplayName)
+                            try {
+                                $SAHash['SendAs'] = ((Get-Mailbox $_.distinguishedName -ErrorAction SilentlyContinue).DisplayName)
+                                if ($SAHash.SendAs) {
+                                    $resultArray += [psCustomObject]$SAHash
+                                }
+                            }
+                            Catch {
+
+                            }
+                        }
+                    }
+                } 
+            } 
+        }
+    }
+    End {
+        $resultArray | Sort -Unique -Property Mailbox, SendAs
+    }
+}
+
 function Get-PermissionsSOB {
     [CmdletBinding()]
     Param 
@@ -81,7 +142,7 @@ function Get-PermissionsSOB {
             [string]$SendOnBehalf = (Get-Mailbox $Mailbox.DistinguishedName | select-object -ExpandProperty GrantSendOnBehalfTo).distinguishedName -join "*"
             if ($SendOnBehalf) {
                 ($SendOnBehalf).split("*") | % {
-                    $ADType = (Get-ADAccountType -name $_ -Sob)
+                    $ADType = (Get-ADAccountType -name $_ -SOB)
                     if ($ADType -eq 'User') {
                         $SOBHash = @{}
                         $SOBHash['Mailbox'] = ($Mailbox.DisplayName)
@@ -124,9 +185,11 @@ function Get-PermissionsSOB {
 function Get-ADAccountType {
     param (
         [string] $Name,
-        [switch] $Sob
+        [switch] $FA,
+        [switch] $SOB,
+        [switch] $SA
     )
-    if (!$Sob) {
+    if ($FA) {
         if ($Name.Contains('\')) {
             $Domain = $Name.Split('\')[0]
             $Name = $Name.Split('\')[1]
@@ -143,9 +206,14 @@ function Get-ADAccountType {
             $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
         }
     }
-    if ($Sob) {
+    if ($SA) {
+        $strFilter = "(&(objectCategory=*)(canonicalname=$Name))"
+        $Domain = $name.Split("/",2)[0]
+        $objSearcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]"LDAP://$Domain")
+    }
+    if ($SOB) {
         $strFilter = "(&(objectCategory=*)(distinguishedname=$Name))"
-        $Domain = (($name -Split '(,DC=)',2)[2]).Replace(',DC=', ".")
+        $Domain = (($name -Split '(,DC=)', 2)[2]).Replace(',DC=', ".")
         $objSearcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]"LDAP://$Domain")
     }
 
--- PSConsult/New-CO_UPNandPrimary.ps1
+++
@@ -1,5 +0,0 @@
-$Mailboxes = Import-Csv .\NewUPNs.csv
-
-Foreach ($Mailbox in $Mailboxes) {
-    get-MsolUser -UserPrincipalName $Mailbox.UPN | Set-MsolUserPrincipalName -NewUserPrincipalName $Mailbox.NewUPN
-}
