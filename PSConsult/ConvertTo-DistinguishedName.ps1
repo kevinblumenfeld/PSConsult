ca2bfc3f256dc8931d965b9bee046efa46a30533
kevinblumenfeld <github@thenext.net>
2017-09-13 12:54:26 -0400

    modified few files and removed CSV

---
+++ PSConsult/ConvertTo-DistinguishedName.ps1
@@ -0,0 +1,58 @@
+﻿<#
+.Synopsis
+   Converts an AD canonical name into a distinguished name.
+.DESCRIPTION
+   Converts an AD canonical name into a distinguished name, and handles
+   the case where USERS, COMPUTERS etc. are containers not OUs
+   and need to be CN= instead of OU=
+.EXAMPLE
+   PS C:\> ConvertTo-DistinguishedName "corp.ad.contoso.com/CORP/Place/on02"
+   
+   CN=on02,OU=Place,OU=CORP,DC=corp,DC=ad,DC=contoso,DC=com
+ #>
+ function ConvertTo-DistinguishedName {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # e.g. 'corp.ad.contoso.com/CORP/USERS/Person'
+        [Parameter(Mandatory = $true,
+            Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        # must be at least a.b/w - no single character domain or missing path
+        [ValidatePattern('^.+\..+/.+$')]
+        $CanonicalName
+    )
+ 
+    $distinguishedNameParts = New-Object -TypeName System.Collections.ArrayList
+ 
+    # separate into domain part (a.b.c.d) and path+user part (/x/y/z/Person)
+    [string]$domain, [array]$remainder = $CanonicalName.Split('/')
+     
+    #
+    # domain:  'a.b.c.d' -> 'DC=a,DC=b,DC=c,DC=d'
+    #
+    $null = $distinguishedNameParts.AddRange($domain.Split('.').ForEach({"DC=$_"}))
+ 
+    $specialContainers = @('Users', 'Computers', 'Builtin', 'ForeignSecurityPrincipals', 'lostAndFound', 'Managed Service Accounts','Program Data', 'Microsoft Exchange System Objects')
+ 
+    0..($remainder.Count - 1) | ForEach-Object {
+        #
+        # handle domain.com/{first} which might be
+        #   OU={first}
+        # or a special container 
+        #   CN={first}
+        #
+        # and handle /Person at the end, which is CN=
+        # all other parts are OU=
+        #
+        $template = if ((($_ -eq 0) -and ($specialContainers -contains $remainder[$_])) -or ($_ -eq ($remainder.Count - 1))) {
+            'CN={0}'
+        }
+        else {
+            'OU={0}'
+        }
+        $null = $distinguishedNameParts.Insert(0, ($template -f $remainder[$_]))
+    }
+    $distinguishedNameParts -join ','
+}
--- PSConsult/Set-UPNtoMatchPrimarySMTP.ps1
+++ PSConsult/Set-UPNtoMatchPrimarySMTP.ps1
@@ -1,5 +1,13 @@
 import-csv c:\scripts\list.csv | 
     ForEach-Object {
-    get-aduser -identity $_.samaccountname -properties SamAccountName, UserPrincipalName, ProxyAddresses | 
-        set-aduser -userprincipalname $_.PrimarySMTPAddress
+    Get-ADUser -identity $_.samaccountname -properties SamAccountName, UserPrincipalName, ProxyAddresses | 
+        Set-ADUser -userprincipalname $_.PrimarySMTPAddress
 }
+
+<#
+Get-ADUser -filter * -Properties ProxyAddresses, SamAccountName, UserPrincipalName | 
+    ? {($_.ProxyAddresses -ne $null) -and !($_.SamAccountName).startswith("SM_")} |
+    Select SamAccountName, @{n = "PrimarySMTP" ; e = {( $_.ProxyAddresses | ? {$_ -cmatch "SMTP:*"}).Substring(5)}} | % {
+    Set-ADUser $_.SamAccountName -UserPrincipalName $_.PrimarySMTP
+}
+#>
