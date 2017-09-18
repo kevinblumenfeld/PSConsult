72fecc28d8f2cb016af96e009c6a4f9c046ccc70
kevinblumenfeld <github@thenext.net>
2017-09-18 17:27:35 -0400

    syntax changes

--- PSConsult/Add-SecondaryProxy.ps1
+++ PSConsult/Add-SecondaryProxy.ps1
@@ -1,6 +1,6 @@
-# Get-ADGroup  -Filter 'proxyaddresses -ne "$null"' -SearchBase "OU=Distribution Groups,DC=GALLS,DC=local" -properties displayname,SamAccountName,mailnickname  | Select DisplayName, SamAccountName, Mailnickname | Export-Csv c:\scripts\dgs.csv -NoTypeInformation
+# Get-ADGroup  -Filter 'proxyaddresses -ne "$null"' -SearchBase "OU=Distribution Groups,DC=CONTOSO,DC=local" -properties displayname,SamAccountName,mailnickname  | Select DisplayName, SamAccountName, Mailnickname | Export-Csv c:\scripts\dgs.csv -NoTypeInformation
 import-csv c:\scripts\dgs.csv | 
     ForEach-Object {
     get-adgroup -identity $_.SamAccountName |? {$ProxyAddresses -ne "$null"} | 
-        Set-ADGroup -add @{ proxyaddresses = ("smtp:" + $($_.mailnickname) + "@GallsLLC.onmicrosoft.com") }
+        Set-ADGroup -add @{ proxyaddresses = ("smtp:" + $($_.mailnickname) + "@contoso.onmicrosoft.com") }
 }
