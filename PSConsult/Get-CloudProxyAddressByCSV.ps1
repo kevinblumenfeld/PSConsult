ce8edf6df1b6dd57f167b7aff5c77e66196fe10f
Kevin Blumenfeld <github@thenext.net>
2017-10-07 18:10:37 -0400

    Update Get-CloudProxyAddressByCSV.ps1

--- PSConsult/Get-CloudProxyAddressByCSV.ps1
+++ PSConsult/Get-CloudProxyAddressByCSV.ps1
@@ -3,9 +3,9 @@ $mailboxes = Import-Csv .\cloudupn.csv
 foreach ($mailbox in $mailboxes) {
     $each = Get-mailbox -identity $mailbox.userprincipalname |
     select DisplayName, samaccountname, Alias, PrimarySmtpAddress,
-@{n = "smtp" ; e = {( $_.EmailAddresses | ? {$_ -cmatch "smtp:*"}). Substring(5 ) -join ";" }},
-@{n = "SIP" ; e = {( $_.EmailAddresses | ? {$_ -like "SIP:*"}). Substring(4 ) -join ";" }},
-@{n = "x500" ; e = {( $_.EmailAddresses | ? {$_ -like "X500:*"}). Substring(0 ) -join ";" }} 
+@{n = "smtp" ; e = {( $_.EmailAddresses | ? {$_ -cmatch "smtp:*"}).Substring(5) -join ";" }},
+@{n = "SIP" ; e = {( $_.EmailAddresses | ? {$_ -like "SIP:*"}).Substring(4) -join ";" }},
+@{n = "x500" ; e = {( $_.EmailAddresses | ? {$_ -like "X500:*"}).Substring(0) -join ";" }} 
     $resultArray += $all
 }
     $resultArray #Export-Csv 365mailboxes.csv -NTI
