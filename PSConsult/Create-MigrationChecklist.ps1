02091583d3971c9c11c278f0c245042775600c64
kevinblumenfeld <github@thenext.net>
2017-10-10 22:29:28 -0400

    used  for readability

--- PSConsult/Create-MigrationChecklist.ps1
+++ PSConsult/Create-MigrationChecklist.ps1
@@ -1,1 +1,6 @@
-Get-MsolUser -All | select DisplayName,Title,Office,Department,FirstName,LastName,StreetAddress,City,State,PostalCode,Country,MobilePhone,PhoneNumber,Fax,UserPrincipalName,UserType,WhenCreated,BlockCredential,CloudExchangeRecipientDisplayType,UsageLocation,IsLicensed,LastDirSyncTime,LastPasswordChangeTimestamp,MSExchRecipientTypeDetails,ImmutableId 
+$properties = @('DisplayName','Title','Office','Department','FirstName','LastName','StreetAddress'
+                'City','State','PostalCode','Country','MobilePhone','PhoneNumber','Fax','UserPrincipalName'
+                'UserType','WhenCreated','BlockCredential','CloudExchangeRecipientDisplayType','UsageLocation'
+                'IsLicensed','LastDirSyncTime','LastPasswordChangeTimestamp','MSExchRecipientTypeDetails','ImmutableId')
+
+Get-MsolUser -All | select $properties
