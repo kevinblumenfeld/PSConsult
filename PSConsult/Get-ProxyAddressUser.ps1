457572f53d66d1852e93bfbbed1f94a50ea3cabd
kevinblumenfeld <github@thenext.net>
2017-10-19 16:50:16 -0400

    added missing comma

--- PSConsult/Get-ProxyAddressUser.ps1
+++ PSConsult/Get-ProxyAddressUser.ps1
@@ -11,7 +11,7 @@
     'UserPrincipalName', 'proxyAddresses'
     'Distinguishedname', 'legacyExchangeDN')
 Get-ADUser -Filter 'proxyaddresses -ne "$null"' -ResultSetSize $null -Properties $Properties -searchBase (Get-ADDomain).distinguishedname -SearchScope SubTree |
-    select DisplayName, samaccountname, UserPrincipalName, legacyExchangeDN
+    select DisplayName, samaccountname, UserPrincipalName, legacyExchangeDN,
 @{n = "OU" ; e = {$_.Distinguishedname | ForEach-Object {($_ -split '(OU=)',2)[1,2] -join ''}}},
 @{n = "PrimarySMTPAddress" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "SMTP:*"}).Substring(5) -join ";" }},
 @{n = "smtp" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "smtp:*"}).Substring(5) -join ";" }},
