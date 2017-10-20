afac801253c86c0c9dd5fb3906a20a5d047d20be
kevinblumenfeld <github@thenext.net>
2017-10-20 11:52:07 -0400

    ADUser detailed

---
+++ PSConsult/Get-ADUserDetailed.ps1
@@ -0,0 +1,32 @@
+<#
+    .SYNOPSIS
+    Export key attributes for any ADUSer that has data in the proxyaddresses attribute.
+
+    .EXAMPLE
+    .\Get-ADUserDetailed.ps1 | Export-Csv .\ADUsersDetailed.csv -notypeinformation
+    
+    #>
+
+    $properties = @('DisplayName', 'Title', 'Office', 'Department', 'Division'
+    'Company', 'Organization', 'EmployeeID', 'EmployeeNumber', 'Description', 'GivenName'
+    'Surname', 'StreetAddress', 'City', 'State', 'PostalCode', 'Country', 'countryCode'
+    'POBox', 'MobilePhone', 'OfficePhone', 'HomePhone', 'Fax', 'cn'
+    'mailnickname','samaccountname', 'UserPrincipalName', 'proxyAddresses'
+    'Distinguishedname', 'legacyExchangeDN', 'EmailAddress')
+
+$Selectproperties = @('DisplayName', 'Title', 'Office', 'Department', 'Division'
+    'Company', 'Organization', 'EmployeeID', 'EmployeeNumber', 'Description', 'GivenName'
+    'Surname', 'StreetAddress', 'City', 'State', 'PostalCode', 'Country', 'countryCode'
+    'POBox', 'MobilePhone', 'OfficePhone', 'HomePhone', 'Fax', 'cn'
+    'mailnickname','samaccountname', 'UserPrincipalName', 'Distinguishedname',
+    'legacyExchangeDN', 'EmailAddress')
+
+$CalculatedProps = @(@{n = "OU" ; e = {$_.Distinguishedname | ForEach-Object {($_ -split '(OU=)', 2)[1, 2] -join ''}}},
+    @{n = "PrimarySMTPAddress" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "SMTP:*"}).Substring(5) -join ";" }},
+    @{n = "smtp" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "smtp:*"}).Substring(5) -join ";" }},
+    @{n = "x500" ; e = {( $_.proxyAddresses | ? {$_ -match "x500:*"}).Substring(0) -join ";" }},
+    @{n = "SIP" ; e = {( $_.proxyAddresses | ? {$_ -match "SIP:*"}).Substring(4) -join ";" }})   
+
+Get-ADUser -Filter 'proxyaddresses -ne "$null"' -ResultSetSize $null -Properties $Properties -searchBase (Get-ADDomain).distinguishedname -SearchScope SubTree |
+    select ($Selectproperties + $CalculatedProps)
+
