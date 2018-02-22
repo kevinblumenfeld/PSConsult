f48aea31e8df3c4f69956a0993d6a8e365bd3bb9
kevinblumenfeld <github@thenext.net>
2018-02-22 16:58:58 -0500

    Exchange based and non schema extended group export

--- PSConsult/Groups/Get-ADGroupDetailed.ps1
+++ PSConsult/Groups/Get-ADGroupDetailed.ps1
@@ -7,26 +7,18 @@
     
     #>
 
-$properties = @('Description', 'DisplayName', 'DistinguishedName', 'dLMemSubmitPerms'
-    'dLMemSubmitPermsBL', 'GroupScope', 'groupType', 'mail', 'mailNickname', 'ManagedBy'
-    'Members', 'msExchBypassAudit', 'msExchGroupDepartRestriction', 'msExchGroupJoinRestriction'
-    'msExchMailboxAuditEnable', 'msExchMailboxAuditLogAgeLimit', 'msExchModerationFlags'
-    'msExchProvisioningFlags', 'msExchRecipientDisplayType', 'msExchRequireAuthToSendTo'
-    'Name', 'proxyAddresses', 'reportToOriginator', 'SamAccountName', 'TargetAddress')
+$properties = @('Description', 'DisplayName', 'DistinguishedName'
+    'GroupScope', 'groupType', 'mail', 'ManagedBy'
+    'Members', 'Name', 'proxyAddresses', 'SamAccountName')
 
 $Selectproperties = @('Description', 'DisplayName', 'GroupScope', 'groupType', 'mail'
-    'mailNickname', 'ManagedBy', 'msExchBypassAudit', 'msExchGroupDepartRestriction'
-    'msExchGroupJoinRestriction', 'msExchMailboxAuditEnable', 'msExchMailboxAuditLogAgeLimit'
-    'msExchModerationFlags', 'msExchProvisioningFlags', 'msExchRecipientDisplayType'
-    'msExchRequireAuthToSendTo', 'Name', 'reportToOriginator', 'SamAccountName', 'TargetAddress')
+    'ManagedBy', 'Name', 'SamAccountName')
 
 
 $CalculatedProps = @(@{n = "OU" ; e = {$_.Distinguishedname | ForEach-Object {($_ -split '(OU=)', 2)[1, 2] -join ''}}},
     @{n = "PrimarySMTPAddress" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "SMTP:*"}).Substring(5) -join ";" }},
     @{n = "smtp" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "smtp:*"}).Substring(5) -join ";" }},
     @{n = "x500" ; e = {( $_.proxyAddresses | ? {$_ -match "x500:*"}).Substring(0) -join ";" }},
-    @{n = "dLMemSubmitPerms" ; e = {($_.dLMemSubmitPerms | ? {$_ -ne $null}) -join ";" }},
-    @{n = "dLMemSubmitPermsBL" ; e = {($_.dLMemSubmitPermsBL | ? {$_ -ne $null}) -join ";" }},
     @{n = "Members" ; e = {($_.Members | ? {$_ -ne $null}) -join ";" }})
 
 Get-ADGroup -Filter 'proxyaddresses -ne "$null"' -Properties $Properties -searchBase (Get-ADDomain).distinguishedname -SearchScope SubTree |
---
+++ PSConsult/Groups/Get-ADGroupDetailedExchange.ps1
@@ -0,0 +1,33 @@
+<#
+    .SYNOPSIS
+    Export key attributes for any ADGroup that has data in the proxyaddresses attribute.
+    Does not contain member export/import yet.
+    .EXAMPLE
+    .\Get-ADGroupDetailed.ps1 | Export-Csv .\ADGroupDetailed.csv -notypeinformation -Encoding UTF8
+    
+    #>
+
+$properties = @('Description', 'DisplayName', 'DistinguishedName', 'dLMemSubmitPerms'
+    'dLMemSubmitPermsBL', 'GroupScope', 'groupType', 'mail', 'mailNickname', 'ManagedBy'
+    'Members', 'msExchBypassAudit', 'msExchGroupDepartRestriction', 'msExchGroupJoinRestriction'
+    'msExchMailboxAuditEnable', 'msExchMailboxAuditLogAgeLimit', 'msExchModerationFlags'
+    'msExchProvisioningFlags', 'msExchRecipientDisplayType', 'msExchRequireAuthToSendTo'
+    'Name', 'proxyAddresses', 'reportToOriginator', 'SamAccountName', 'TargetAddress')
+
+$Selectproperties = @('Description', 'DisplayName', 'GroupScope', 'groupType', 'mail'
+    'mailNickname', 'ManagedBy', 'msExchBypassAudit', 'msExchGroupDepartRestriction'
+    'msExchGroupJoinRestriction', 'msExchMailboxAuditEnable', 'msExchMailboxAuditLogAgeLimit'
+    'msExchModerationFlags', 'msExchProvisioningFlags', 'msExchRecipientDisplayType'
+    'msExchRequireAuthToSendTo', 'Name', 'reportToOriginator', 'SamAccountName', 'TargetAddress')
+
+
+$CalculatedProps = @(@{n = "OU" ; e = {$_.Distinguishedname | ForEach-Object {($_ -split '(OU=)', 2)[1, 2] -join ''}}},
+    @{n = "PrimarySMTPAddress" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "SMTP:*"}).Substring(5) -join ";" }},
+    @{n = "smtp" ; e = {( $_.proxyAddresses | ? {$_ -cmatch "smtp:*"}).Substring(5) -join ";" }},
+    @{n = "x500" ; e = {( $_.proxyAddresses | ? {$_ -match "x500:*"}).Substring(0) -join ";" }},
+    @{n = "dLMemSubmitPerms" ; e = {($_.dLMemSubmitPerms | ? {$_ -ne $null}) -join ";" }},
+    @{n = "dLMemSubmitPermsBL" ; e = {($_.dLMemSubmitPermsBL | ? {$_ -ne $null}) -join ";" }},
+    @{n = "Members" ; e = {($_.Members | ? {$_ -ne $null}) -join ";" }})
+
+Get-ADGroup -Filter 'proxyaddresses -ne "$null"' -Properties $Properties -searchBase (Get-ADDomain).distinguishedname -SearchScope SubTree |
+    select ($Selectproperties + $CalculatedProps)
