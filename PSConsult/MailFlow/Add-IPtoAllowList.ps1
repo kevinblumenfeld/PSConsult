56ec60de61ea576c631736c37cfcc15b965f3320
kevinblumenfeld <github@thenext.net>
2018-03-05 17:32:19 -0500

    help tabs

--- PSConsult/MailFlow/Add-IPtoAllowList.ps1
+++ PSConsult/MailFlow/Add-IPtoAllowList.ps1
@@ -1,32 +1,32 @@
 function Add-IPtoAllowList {
     <#
 	.SYNOPSIS
-		Adds a list of IP Addresses to a Hosted Connection Filter Policy.  If the policy does not exist it creates it.
+        Adds a list of IP Addresses to a Hosted Connection Filter Policy.  If the policy does not exist it creates it.
 	
 	.DESCRIPTION
-		Adds a list of IP Addresses to a Hosted Connection Filter Policy.  If the policy does not exist it creates it.
+        Adds a list of IP Addresses to a Hosted Connection Filter Policy.  If the policy does not exist it creates it.
 	
 	.PARAMETER IPs
-            The IP addresses to be added to the Allow List of a Hosted Connection Filter Policy.
+        The IP addresses to be added to the Allow List of a Hosted Connection Filter Policy.
 
-            You enter the IP addresses using the following syntax:
+        You enter the IP addresses using the following syntax:
 
-            Single IP   For example, 192.168.1.1
-            IP range   You can use an IP address range, for example, 192.168.0.1-192.168.0.254
-            CIDR IP   You can use Classless InterDomain Routing (CIDR), for example, 192.168.0.1/25
+        Single IP   For example, 192.168.1.1
+        IP range   You can use an IP address range, for example, 192.168.0.1-192.168.0.254
+        CIDR IP   You can use Classless InterDomain Routing (CIDR), for example, 192.168.0.1/25
 	
 	.PARAMETER ConnectionFilterPolicy
-		Name of the Connection Filter Policy to use.  If Connection Filter Policy does not exist it will be created.
+        Name of the Connection Filter Policy to use.  If Connection Filter Policy does not exist it will be created.
 	
 	.PARAMETER OutputPath
-		Where to write the report files to.
-		By default it will write to the current path.
-	
+        Where to write the report files to.
+        By default it will write to the current path.
+
 	.EXAMPLE
         Import-Csv .\IP.csv | Add-IPtoAllowList -ConnectionFilterPolicy "IP Addresses of Partners"
                 
         Example of IP.csv
-        
+
         IP
         64.43.44.42
         68.44.41.41
