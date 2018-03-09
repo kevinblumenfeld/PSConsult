fe4739b325f32710efbe20d29781c91970977f45
kevinblumenfeld <github@thenext.net>
2018-03-09 17:42:10 -0500

    Tested version

--- PSConsult/MailFlow/Add-TransportRuleDetails.ps1
+++ PSConsult/MailFlow/Add-TransportRuleDetails.ps1
@@ -5,7 +5,11 @@ function Add-TransportRuleDetails {
 	
 	.DESCRIPTION
         Adds details to Transport Rule.  If the transport rule does not exist it creates it.
-	
+
+    
+	.PARAMETER TransportRule
+        Name of the Transport Rule to use.  If Transport Rule does not exist it will be created.
+		
 	.PARAMETER RecipientAddressContainsWords
         This parameter specifies a condition or part of a condition for the rule. The name of the corresponding exception parameter starts with ExceptIf.
 
@@ -24,36 +28,67 @@ function Add-TransportRuleDetails {
         You can specify multiple words separated by commas. This parameter works when the recipient is an individual user.
         This parameter doesn't work with distribution groups.
 
-        .PARAMETER SubjectOrBodyContainsWords
+    .PARAMETER SubjectOrBodyContainsWords
         This parameter specifies a condition or part of a condition for the rule. The name of the corresponding exception parameter starts with ExceptIf.
 
         In on-premises Exchange, this condition is available on Mailbox servers and Edge Transport servers.
 
         The SubjectOrBodyContainsWords parameter specifies a condition that looks for words in the Subject field or body of messages.
 
-        .PARAMETER ExceptIfSubjectOrBodyContainsWords
+    .PARAMETER ExceptIfSubjectOrBodyContainsWords
         This parameter specifies an exception or part of an exception for the rule. The name of the corresponding condition doesn't include the ExceptIf prefix.
 
         In on-premises Exchange, this exception is available on Mailbox servers and Edge Transport servers.
 
         The ExceptIfSubjectOrBodyContainsWords parameter specifies an exception that looks for words in the Subject field or body of messages.
 
-	.PARAMETER TransportRule
-        Name of the Transport Rule to use.  If Transport Rule does not exist it will be created.
-	
+    .PARAMETER SenderIPRanges
+        This parameter specifies a condition or part of a condition for the rule. The name of the corresponding exception parameter starts with ExceptIf.
+
+        In on-premises Exchange, this condition is only available on Mailbox servers.
+
+        The SenderIpRanges parameter specifies a condition that looks for senders whose IP addresses matches the specified value, or fall within the specified ranges. Valid values are:
+
+          Single IP address   For example, 192.168.1.1.
+          IP address range   For example, 192.168.0.1-192.168.0.254.
+          Classless InterDomain Routing (CIDR) IP address range   For example, 192.168.0.1/25.
+
+        You can specify multiple IP addresses or ranges separated by commas.
+
+    .PARAMETER AttachmentContainsWords
+        This parameter specifies a condition or part of a condition for the rule. The name of the corresponding exception parameter starts with ExceptIf.
+
+        In on-premises Exchange, this condition is only available on Mailbox servers.
+
+        The AttachmentContainsWords parameter specifies a condition that looks for words in message attachments. Only supported attachment types are checked.
+
+        To specify multiple words or phrases, this parameter uses the syntax: Word1,"Phrase with spaces",word2,.... Don't use leading or trailing spaces.
+    
+    .PARAMETER AttachmentMatchesPatterns
+        This parameter specifies a condition or part of a condition for the rule. The name of the corresponding exception parameter starts with ExceptIf.
+
+        In on-premises Exchange, this condition is only available on Mailbox servers.
+
+        The AttachmentMatchesPatterns parameter specifies a condition that looks for text patterns in the content of message attachments by using regular expressions. Only supported attachment types are checked.
+
+        You can specify multiple text patterns by using the following syntax: "<regular expression1>","<regular expression2>",....
+
 	.PARAMETER OutputPath
         Where to write the report files to.
         By default it will write to the current path.
 
 	.EXAMPLE
-        Import-Csv .\RuleDetails.csv | Add-TransportRuleDetails -TransportRule "Block Macros Except when Certain Words are used"
-                
+        Import-Csv .\RuleDetails.csv | Add-TransportRuleDetails -TransportRule "Block Macros Except when Certain Words are used" -Action01 DeleteMessage
+
         Example of RuleDetails.csv
 
-        AddressWords, SubjectWords
-        fred@contoso.com, moon 
-        jane@fabrikam.com, sun fire
-        potato.com, ocean
+        AddressWords, SubjectBodyWords, ExceptSubjectBodyWords, SenderIPs 
+        fred@contoso.com, moon, wind, 142.23.221.21
+        jane@fabrikam.com, sun fire, rain, 142.23.220.1-142.23.220.254
+        potato.com, ocean, snow, 72.14.52.0/24
+    .EXAMPLE
+        Import-Csv .\RuleDetails.csv | Add-TransportRuleDetails -TransportRule "Bypass Spam Filtering for New York Partners" -Action01 BypassSpamFiltering
+
 #>
     [CmdletBinding()]
     param (
@@ -62,40 +97,46 @@ function Add-TransportRuleDetails {
         [String]
         $TransportRule,
 
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('AddressWords')]
         [string[]]
         $RecipientAddressContainsWords,
         
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('ExceptAddressWords')]
         [string[]]
         $ExceptIfRecipientAddressContainsWords,
         
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('SubjectBodyWords')]
         [Alias('SBWords')]
         [string[]]
         $SubjectOrBodyContainsWords,
         
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('ExceptSubjectBodyWords')]
-        [Alias('ExceptWords')]
+        [Alias('ExceptSBWords')]
         [string[]]
         $ExceptIfSubjectOrBodyContainsWords,
+        
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [Alias('SenderIP')]
+        [Alias('SenderIPs')]
+        [string[]]
+        $SenderIPRanges,
 
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('AttachmentWords')]
         [string[]]
         $AttachmentContainsWords,
                 
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
         [Alias('AttachmentPatterns')]
         [string[]]
         $AttachmentMatchesPatterns,
         
         [Parameter(Mandatory = $true)]
-        [ValidateSet("DeleteMessage", "NotifyRecipients")]
+        [ValidateSet("DeleteMessage", "BypassSpamFiltering")]
         $Action01,
 
         [string]
@@ -103,7 +144,14 @@ function Add-TransportRuleDetails {
     )
     begin {
         $Params = @{}
-        $listAddressWords = New-Object System.Collections.Generic.List[System.Object]
+        $listAddressWords = New-Object System.Collections.Generic.List[String]
+        $listExceptAddressWords = New-Object System.Collections.Generic.List[String]
+        $listSBWords = New-Object System.Collections.Generic.List[String]
+        $listExceptSBWords = New-Object System.Collections.Generic.List[String]
+        $listAttachmentWords = New-Object System.Collections.Generic.List[String]
+        $listAttachmentPattern = New-Object System.Collections.Generic.List[String]
+        $listSenderIPRanges = New-Object System.Collections.Generic.List[String]
+
         $headerstring = ("TransportRule" + "," + "Details")
         $errheaderstring = ("TransportRule" + "," + "Details" + "," + "Error")
 		
@@ -115,31 +163,39 @@ function Add-TransportRuleDetails {
     }
     process {
         if ($RecipientAddressContainsWords) {
-                $listAddressWords.add($RecipientAddressContainsWords)
-                # Write-Host $RecipientAddressContainsWords
+            $listAddressWords.add($RecipientAddressContainsWords)
         }
         if ($ExceptIfRecipientAddressContainsWords) {
-
+            $listExceptAddressWords.add($ExceptIfRecipientAddressContainsWords)
         }
         if ($SubjectOrBodyContainsWords) {
-
+            $listSBWords.add($SubjectOrBodyContainsWords)
         }
         if ($ExceptIfSubjectOrBodyContainsWords) {
-
+            $listExceptSBWords.add($ExceptIfSubjectOrBodyContainsWords)
         }
         if ($AttachmentContainsWords) {
-
+            $listAttachmentWords.add($AttachmentContainsWords)
         }
         if ($AttachmentMatchesPatterns) {
-
+            $listAttachmentPattern.add($AttachmentMatchesPatterns)
+        }        
+        if ($SenderIPRanges) {
+            $listSenderIPRanges.add($SenderIPRanges)
         }
     }
     end {
         if ($Action01 -eq "DeleteMessage") {
             $Params.Add("DeleteMessage", $true)
         }
+        if ($Action01 -eq "BypassSpamFiltering") {
+            $Params.Add("SetSCL", "-1")
+        }
         if ($RecipientAddressContainsWords) {
             $Params.Add("RecipientAddressContainsWords", $listAddressWords)
+        }
+        if ($listSenderIPRanges) {
+            $Params.Add("SenderIPRanges", $listSenderIPRanges)
         }
         if (!(Get-TransportRule -Identity $TransportRule -ErrorAction SilentlyContinue)) {
             Try {
@@ -166,4 +222,3 @@ function Add-TransportRuleDetails {
         }
     }
 }
-# Import-Csv c:\scripts\words.csv | Add-TransportRuleDetails -TransportRule "000" -Action01 DeleteMessage
